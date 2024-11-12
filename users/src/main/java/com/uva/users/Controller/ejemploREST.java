0336c93b44e031e6ccd76fdaec2f935a303340b7
danimar <daniel.martinez.gonzalez@uva.es>
2024-11-12 16:04:59 +0100

    Algunas operaciones CRUD

--- users/src/main/java/com/uva/users/Controller/ejemploREST.java
+++ users/src/main/java/com/uva/users/Controller/ejemploREST.java
@@ -1,14 +1,73 @@
 package com.uva.users.Controller;
 
+import java.util.List;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import com.uva.users.Exception.VinoException;
+import com.uva.users.Model.Vino;
+import com.uva.users.Repository.VinoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("/ejemploVinos")
 class ejemploREST {
-    @GetMapping()
-    public String getVinos() {
-        return "Mensaje desde getVinos";
+
+    private final VinoRepository repository;
+
+    ejemploREST(VinoRepository repository) {
+        this.repository = repository;
     }
+
+    // No se ha incluido el atributo path. Si en el ejemplo que tienes está,
+    // añadirle
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String newVino(@RequestBody Vino newVino) {
+        try {
+            repository.save(newVino);
+            return "Nuevo registro creado";
+        } catch (Exception e) {
+            // Se deja esta parte comentada como alternativa a la gestión de errores
+            // propuesta
+            // throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Error
+            // alcrear el nuevo registro.");
+            // Se usa este sistema de gestión de errores porque es más sencillo hacer que el
+            // cliente reciba el mensaje de error
+            throw new VinoException("Error al crear el nuevo registro.");
+        }
+    }
+
+    @GetMapping()
+    public List<Vino> getVinos() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Vino getVinos(@PathVariable() Integer id) {
+        Vino vino = repository.findById(id).orElseThrow(() -> new VinoException("Sin resultado"));
+        return vino;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVino(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String putVino(@PathVariable Integer id, @RequestBody Vino newVino) {
+        Vino oldBVino = repository.findById(id).orElseThrow(() -> new VinoException("Sin resultado"));
+        newVino.setId(oldBVino.getId());
+        repository.save(newVino);
+        return "Registro Actualizado";
+    }
+    
 
     @PutMapping("/id")
     public String putVinos(@RequestBody String body) {
@@ -21,14 +80,16 @@ class ejemploREST {
                 ". Contenido del cuerpo de la petición: " + body;
     }
 
-   /* @GetMapping(value = { "/cartaVinos", "/cartaVinos/{nombre}" })
-    public String getCarta(@PathVariable(required = false) String nombre) {
-        if (nombre != null) {
-            return "vino con nombre: " + nombre;
-        } else {
-            return "Toda la carta de vinos";
-        }
-    }*/
+    /*
+     * @GetMapping(value = { "/cartaVinos", "/cartaVinos/{nombre}" })
+     * public String getCarta(@PathVariable(required = false) String nombre) {
+     * if (nombre != null) {
+     * return "vino con nombre: " + nombre;
+     * } else {
+     * return "Toda la carta de vinos";
+     * }
+     * }
+     */
 
     @GetMapping("/cartaVinos")
     public String getCartaConQuery(@RequestParam String nombre) {
--- users/src/main/java/com/uva/users/Model/Vino.java
+++ users/src/main/java/com/uva/users/Model/Vino.java
@@ -1,5 +1,6 @@
 package com.uva.users.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,6 +16,7 @@ public class Vino {
     private String nombre_comercial;
     private String denominacion;
     private String categoria;
+    @Column(nullable = false)
     private Float precio;
     private Integer bodega_id;
 
