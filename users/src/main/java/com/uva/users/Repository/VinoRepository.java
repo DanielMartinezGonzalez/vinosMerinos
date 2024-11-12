39918628b360fdc7e34b0bc7c622a034d7be58ea
danimar <daniel.martinez.gonzalez@uva.es>
2024-11-12 15:20:11 +0100

    El repositprio

---
+++ users/src/main/java/com/uva/users/Repository/VinoRepository.java
@@ -0,0 +1,9 @@
+package com.uva.users.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uva.users.Model.Vino;
+
+public interface VinoRepository extends JpaRepository<Vino, Integer>{
+
+}
